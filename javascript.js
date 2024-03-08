4c2d48606eb6ad987ad981ecc134cb9ff2297f37
justutpal <kumarutpal2112001@gmail.com>
2024-03-08 20:22:56 +0530

    Finally all work and function done here

--- javascript.js
+++ javascript.js
@@ -1,13 +1,26 @@
-let height = document.querySelector("#height.value");
-let weight = document.querySelector("#weight.value");
+let inputHeight = document.querySelector("#height");
+let inputWeight = document.querySelector("#weight");
 let submit = document.querySelector("#submit");
 let result = document.querySelector("#result");
-let form = document.querySelectorAll("form input")
-console.log(form.value)
-console.log(weight)
+let para = document.querySelector("#para");
+
 
 // Calculate BMI
-
-let BMI = function(height, weight) {
-    weight / ((height * height) / 10000)
+let text = ""
+let bmi = function() {
+   text = inputWeight.value / ((inputHeight.value * inputHeight.value) / 10000)
+   let finalResult = result.innerHTML = text
+   if (finalResult < 18.5) {
+      para.style.color = "red"
+      para.innerHTML = "You are UNDERWEIGHT &#128542;"
+   }
+   else if (finalResult >= 18.5 && finalResult <= 24.9){
+      para.style.color = "green"
+      para.innerHTML = "You are PERFECT &#128525;"
+   }
+   else if ( finalResult > 24.9 ){
+      para.style.color = "red"
+      para.innerHTML = "You are OVERWEIGHT &#128551;"
+   }
 }
+submit.addEventListener('click', bmi)
